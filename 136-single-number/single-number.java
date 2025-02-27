d73d72c347822bae03cb37e6e50d810f1de7c58c
Ramandeep-singh-bhatia <54952924+Ramandeep-singh-bhatia@users.noreply.github.com>
2025-02-26 16:31:10 -0800

    Time: 1 ms (99.86%) | Memory: 46.5 MB (17.22%) - LeetSync

--- 136-single-number/single-number.java
+++ 136-single-number/single-number.java
@@ -1,18 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        // 2*(a + b + c) - (a +a + b + b + c)
-        int sum1 = 0;
-        int sum2 = 0;
-        Set<Integer> set = new HashSet<>();
+        int result = 0;
         for(int num: nums){
-            if(!set.contains(num)){
-                sum1 += num;
-                set.add(num);
-            }
-
-            sum2 += num;
+            result ^= num;
         }
-
-        return 2* sum1 - sum2;
+        return result;
     }
 }
