94f36dd1523e07d894369ef4aff864fe95f5b7e0
Ramandeep-singh-bhatia <54952924+Ramandeep-singh-bhatia@users.noreply.github.com>
2025-04-07 14:04:59 -0700

    Time: 0 ms (100.00%) | Memory: 44.2 MB (94.11%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -1,10 +1,30 @@
 /*
+    Bottom up recursion
+*/
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return height(root) != -1;
+    }
+    
+    private int height(TreeNode node) {
+        if(node == null)
+            return 0;
+        int left = height(node.left);
+        int right = height(node.right);
+        if(left == -1 || right == -1 || Math.abs(left - right) > 1) 
+            return -1; // unbalanced
+        return Math.max(left, right) + 1;
+    }
+}
+
+/*
     Top down approach
      Time - O(n^2)
      Space - o(n)
 */
 
-class Solution {
+/*class Solution {
     public boolean isBalanced(TreeNode root) {
         if(root == null)
             return true;
@@ -21,4 +41,4 @@ class Solution {
             return 0;
         return Math.max(height(node.left), height(node.right)) + 1;
     }
-}
+}*/
