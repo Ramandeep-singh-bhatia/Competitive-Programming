5700fe1ce2b2891f6f779f9cb91936556da319ef
Ramandeep-singh-bhatia <54952924+Ramandeep-singh-bhatia@users.noreply.github.com>
2025-04-18 16:51:18 -0700

    Time: 2 ms (88.05%) | Memory: 56.6 MB (6.41%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.java
+++ 238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,4 +1,10 @@
-class Solution {
+/*
+    Naive Solution
+    Time O(n)
+    Space O(n) - result array
+*/
+
+/*class Solution {
     public int[] productExceptSelf(int[] nums) {
         int n = nums.length;
         int product = 1;
@@ -20,6 +26,39 @@ class Solution {
                 result[i] = product/nums[i];
             }
         }
+        return result;
+    }
+}*/
+
+/*
+    Prefix/suffix product solution. 
+    3 pass.
+    Time - O(n)
+    Space - O(n)
+*/
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+        int[] result = new int[n];
+
+        prefix[0] = 1;
+        suffix[n-1] = 1;
+
+        for(int i = 1; i < n; i++){
+            prefix[i] = prefix[i-1] * nums[i-1];
+        }
+
+        for(int i = n-2; i >= 0; i--){
+            suffix[i] = suffix[i+1] * nums[i+1];
+        }
+
+        for(int i = 0; i < n; i++){
+            result[i] = prefix[i] * suffix[i]; 
+        }
+
         return result;
     }
 }
