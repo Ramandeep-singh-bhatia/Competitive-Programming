94ffdde65b0574804a10a8d6ae99bc852e493c13
Ramandeep-singh-bhatia <54952924+Ramandeep-singh-bhatia@users.noreply.github.com>
2025-04-18 17:17:06 -0700

    Time: 42 ms (60.38%) | Memory: 49.7 MB (24.92%) - LeetSync

--- 346-moving-average-from-data-stream/moving-average-from-data-stream.java
+++ 346-moving-average-from-data-stream/moving-average-from-data-stream.java
@@ -2,19 +2,18 @@ class MovingAverage {
     Queue<Integer> q;
     int size, sum = 0, count = 0;
     public MovingAverage(int size) {
-        q = new LinkedList<>();
+        q = new ArrayDeque<>();
         this.size = size;
     }
     
     public double next(int val) {
-        count++;
         q.add(val);
 
-        int end = count > this.size ? q.poll() : 0;
+        int discard = q.size() > this.size ? q.poll() : 0;
 
-        sum += val - end;
+        sum += val - discard;
 
-        return (double) sum / Math.min(count, this.size);
+        return (double) sum / Math.min(q.size(), this.size);
     }
 }
 
