53a5f66c65405426035ea6dc2bc178df5b12b9f3
Ramandeep-singh-bhatia <54952924+Ramandeep-singh-bhatia@users.noreply.github.com>
2025-04-24 13:57:53 -0700

    Time: 7 ms (63.65%) | Memory: 44.8 MB (60.60%) - LeetSync

---
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.java
@@ -0,0 +1,103 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+ /*
+Intuition
+
+The approach is pretty intuitive. Traverse the tree in a depth first manner. The moment you encounter either of the nodes p or q, return some boolean flag. The flag helps to determine if we found the required nodes in any of the paths. The least common ancestor would then be the node for which both the subtree recursions return a True flag. It can also be the node which itself is one of p or q and for which one of the subtree recursions returns a True flag.
+
+Let us look at the formal algorithm based on this idea.
+
+Algorithm
+
+Start traversing the tree from the root node.
+If the current node itself is one of p or q, we would mark a variable mid as True and continue the search for the other node in the left and right branches.
+If either of the left or the right branch returns True, this means one of the two nodes was found below.
+If at any point in the traversal, any two of the three flags left, right or mid become True, this means we have found the lowest common ancestor for the nodes p and q.
+ */
+class Solution {
+    TreeNode ans = null;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        dfs(root, p ,q);
+        return ans;
+    }
+
+    public boolean dfs(TreeNode curr, TreeNode p, TreeNode q) {
+        if(curr == null)
+            return false;
+        
+        boolean left = dfs(curr.left, p ,q);
+
+        boolean right = dfs(curr.right, p ,q);
+
+        boolean mid = (curr == p || curr == q);
+         
+        if((mid && left) || (mid && right) || (left && right))
+            ans = curr;
+        
+        return mid || left || right;
+    }
+}
+
+/*class Solution {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        // Stack for tree traversal
+        //Deque<TreeNode> stack = new ArrayDeque<>();
+        //Stack<TreeNode> stack = new Stack<>();
+        Queue<TreeNode> qu = new LinkedList<>();
+
+        // HashMap for parent pointers
+        Map<TreeNode, TreeNode> parent = new HashMap<>();
+
+        parent.put(root, null);
+        qu.add(root);
+
+        // Iterate until we find both the nodes p and q
+        while (!parent.containsKey(p) || !parent.containsKey(q)) {
+
+            TreeNode node = qu.poll();
+
+            // While traversing the tree, keep saving the parent pointers.
+            if (node.left != null) {
+                parent.put(node.left, node);
+                qu.add(node.left);
+            }
+            if (node.right != null) {
+                parent.put(node.right, node);
+                qu.add(node.right);
+            }
+        }
+
+        TreeNode a = p, b = q;
+        while(a != b){
+            a = a == null ? q : parent.get(a);
+            b = b == null ? p : parent.get(b);
+        }
+        return a;
+
+        // Ancestors set() for node p.
+        /*Set<TreeNode> ancestors = new HashSet<>();
+
+        // Process all ancestors for node p using parent pointers.
+        while (p != null) {
+            ancestors.add(p);
+            p = parent.get(p);
+        }
+
+        // The first ancestor of q which appears in
+        // p's ancestor set() is their lowest common ancestor.
+        while (!ancestors.contains(q))
+            q = parent.get(q);
+        return q;*/
+    /*}
+
+}*/
