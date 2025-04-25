5656b3c297fe07b1864f46269004906836fa6a6b
Ramandeep-singh-bhatia <54952924+Ramandeep-singh-bhatia@users.noreply.github.com>
2025-04-24 19:55:05 -0700

    Time: 66 ms (10.27%) | Memory: 52.6 MB (57.83%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -34,7 +34,7 @@ Space - O(n)
     Space - O(k)
 */
 
-class Solution {
+/*class Solution {
     public int[][] kClosest(int[][] points, int k) {
         Queue<int[]> maxHeap = new PriorityQueue<>((int[] a, int[] b) -> ((b[0] * b[0] + b[1] * b[1]) - (a[0] * a[0] + a[1] * a[1])));
 
@@ -53,51 +53,75 @@ class Solution {
         return result;
 
     }
-}
+}*/
 
-/*class Solution {
-    private int[][] quickSelect(int[][] points, int k){
-        int L = 0, R = points.length - 1;
-        int M = points.length;
-        while (M != k){
-            M = partition(points, L, R);
-            if (M < k){
-                L = M;
-            } else {
-                R = M - 1;
-            }
-        }
+class Solution {
+    private Random random = new Random();
+    
+    public int[][] kClosest(int[][] points, int k) {
+        
+        // Apply quickselect algorithm
+        quickSelect(points, 0, points.length - 1, k);
+        
+        // Return the first k points
         return Arrays.copyOf(points, k);
     }
     
-    private int partition(int[][] points, int L, int R){
-        int[] pivot = choosePivot(points, L, R);
-        int pivotDist = squareDistance(pivot);
-        while (L < R){
-            if (squareDistance(points[L]) >= pivotDist){
-                int[] temp = points[L];
-                points[L] = points[R];
-                points[R] = temp;
-                R--;
-            } else {
-                L++;
+    private void quickSelect(int[][] points, int left, int right, int k) {
+        if (left >= right) {
+            return;
+        }
+        
+        // Partition the array around the pivot
+        int pivotIndex = partition(points, left, right);
+        
+        // If partition index is k, we're done
+        if (pivotIndex == k - 1) {
+            return;
+        } 
+        // If k is on the left of pivot, search the left part
+        else if (pivotIndex > k - 1) {
+            quickSelect(points, left, pivotIndex - 1, k);
+        } 
+        // If k is on the right of pivot, search the right part
+        else {
+            quickSelect(points, pivotIndex + 1, right, k);
+        }
+    }
+    
+    private int partition(int[][] points, int left, int right) {
+
+        // Choose a random pivot
+        int pivotIndex = left + random.nextInt(right - left + 1);
+        // Calculate squared distance of pivot point from origin
+        int pivotDist = distanceSquared(points[pivotIndex]);
+        
+        // Move pivot to the end
+        swap(points, pivotIndex, right);
+        
+        // Move all points with distance less than pivot to the left
+        int storeIndex = left;
+        for (int i = left; i < right; i++) {
+            if (distanceSquared(points[i]) < pivotDist) {
+                swap(points, i, storeIndex);
+                storeIndex++;
             }
         }
         
-        if (squareDistance(points[L]) < pivotDist)
-            L++;
-        return L;
+        // Move pivot to its final place
+        swap(points, storeIndex, right);
+        
+        return storeIndex;
     }
     
-    private int[] choosePivot(int[][] points, int L, int R){
-        return points[L + (R-L) / 2];
-    }
-    
-    private int squareDistance(int[] point){
+    private int distanceSquared(int[] point) {
+        // Calculate squared Euclidean distance from origin (0,0)
         return point[0] * point[0] + point[1] * point[1];
     }
-
-    public int[][] kClosest(int[][] points, int k) {
-        return quickSelect(points, k);
+    
+    private void swap(int[][] points, int i, int j) {
+        int[] temp = points[i];
+        points[i] = points[j];
+        points[j] = temp;
     }
-}*/
+}
