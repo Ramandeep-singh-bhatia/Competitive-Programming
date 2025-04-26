2d180613316cda1f56028dcbf5f2670df667721a
Ramandeep-singh-bhatia <54952924+Ramandeep-singh-bhatia@users.noreply.github.com>
2025-04-26 13:28:49 -0700

    Time: 25 ms (69.48%) | Memory: 42.9 MB (36.58%) - LeetSync

--- 133-clone-graph/clone-graph.java
+++ 133-clone-graph/clone-graph.java
@@ -26,7 +26,7 @@ class Node {
     Space - O(V) For map that stores each node. Queue will also use some space which will be width of the graph but overall it will be O(V)
 */
 
-class Solution {
+/*class Solution {
     public Node cloneGraph(Node node) {
         if(node == null)
             return node;
@@ -52,5 +52,34 @@ class Solution {
         }
         
         return visited.get(node);
+    }
+}*/
+
+class Solution {
+    private Map<Node, Node> visited = new HashMap<>();
+
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+            return node;
+        }
+
+        // If the node was already visited before.
+        // Return the clone from the visited dictionary.
+        if (visited.containsKey(node)) {
+            return visited.get(node);
+        }
+
+        // Create a clone for the given node.
+        // Note that we don't have cloned neighbors as of now, hence [].
+        Node cloneNode = new Node(node.val, new ArrayList());
+        // The key is original node and value being the clone node.
+        visited.put(node, cloneNode);
+
+        // Iterate through the neighbors to generate their clones
+        // and prepare a list of cloned neighbors to be added to the cloned node.
+        for (Node neighbor : node.neighbors) {
+            cloneNode.neighbors.add(cloneGraph(neighbor));
+        }
+        return cloneNode;
     }
 }
