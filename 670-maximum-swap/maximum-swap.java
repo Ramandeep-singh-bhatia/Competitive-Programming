969146e3d1a649354b2066cd696f687deb209369
Ramandeep-singh-bhatia <54952924+Ramandeep-singh-bhatia@users.noreply.github.com>
2025-04-26 01:21:03 -0700

    Time: 0 ms (100.00%) | Memory: 40.5 MB (49.99%) - LeetSync

--- 670-maximum-swap/maximum-swap.java
+++ 670-maximum-swap/maximum-swap.java
@@ -15,10 +15,10 @@ Time - O(n) Overall
        O(n) for looping through the array to populate rightmostindex
        O(n) for outer loop to swap the digit
        O(n) for converting the string back to integer
-Space - O(n) 
+Space - O(n) for arr array
 */
 
-class Solution {
+/*class Solution {
     public int maximumSwap(int num) {
         char[] arr = String.valueOf(num).toCharArray();
 
@@ -41,5 +41,35 @@ class Solution {
 
         return num;
         
+    }
+}*/
+
+/*
+    We don't have to store the rightMostIndex in an array. We can keep track of the rightMostIndex of the maximum digit and also the element smaller to the max digit to the left so that we can swap. This saves some space but it is still O(n) due to array 
+    Time - O(n)
+    Space - O(n)
+*/
+
+class Solution {
+    public int maximumSwap(int num) {
+        char[] arr = String.valueOf(num).toCharArray();
+        int maxIndex = -1, minValue = -1, maxValue = -1;
+
+        for(int i = arr.length-1; i >=0; i--){
+            if(maxIndex == -1 || arr[i] > arr[maxIndex])
+                maxIndex = i;
+            else if (arr[i] < arr[maxIndex]){
+                minValue = i;
+                maxValue = maxIndex;
+            }
+        }
+
+        if(maxValue != -1 && minValue != -1){
+            char temp = arr[minValue];
+            arr[minValue] = arr[maxValue];
+            arr[maxValue] = temp;
+        }
+
+        return Integer.valueOf(new String(arr));
     }
 }
