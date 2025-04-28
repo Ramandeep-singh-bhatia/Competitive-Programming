8e22fc74f94d4870f9f3efbbf42a966edc176b9f
Ramandeep-singh-bhatia <54952924+Ramandeep-singh-bhatia@users.noreply.github.com>
2025-04-28 00:10:21 -0700

    Time: 18 ms (88.60%) | Memory: 61.1 MB (5.42%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -6,13 +6,32 @@ BruteForce
         Space O(1)
 */
 
-class Solution {
+/*class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j - i <= k && j < nums.length; j++) {
                 if (nums[i] == nums[j]) {
                     return true;
                 }
+            }
+        }
+        return false;
+    }
+}*/
+/*
+Hashset
+Time - O(n)
+Space - O(k). if k is = n then O(n)
+*/
+
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; ++i) {
+            if (!set.add(nums[i])) 
+                return true;
+            if (set.size() > k) {
+                set.remove(nums[i - k]);
             }
         }
         return false;
