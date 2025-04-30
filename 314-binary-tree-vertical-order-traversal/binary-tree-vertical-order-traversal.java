0b8951b28d922e9832f572204cd63944ecfb40b9
Ramandeep-singh-bhatia <54952924+Ramandeep-singh-bhatia@users.noreply.github.com>
2025-04-29 20:48:34 -0700

    Time: 3 ms (59.97%) | Memory: 42.8 MB (84.06%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.java
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.java
@@ -13,7 +13,12 @@
  *     }
  * }
  */
-class Solution {
+
+ /*
+    Time - O(N)
+    Space - O(N)
+ */
+/*class Solution {
     public List<List<Integer>> verticalOrder(TreeNode root) {
         Map<Integer, List<Integer>> map = new HashMap<>();
         Queue<Pair<TreeNode, Integer>> q = new LinkedList<>();
@@ -44,5 +49,49 @@ class Solution {
         }
 
         return result;
+    }
+}*/
+
+/*
+    Time - O(W HlogH) - O(W) for the width of the tree which we will loop from incolumn to max. We then sort the list in the map using depth which is height so HlogH for that
+*/
+class Solution {
+    int minColumn = 0, maxColumn = 0;
+    public List<List<Integer>> verticalOrder(TreeNode root) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        if(root == null){
+            return result;
+        }
+        
+        Map<Integer, List<int[]>> map = new TreeMap<>();
+        dfs(root,0,0,map);
+        
+        for(int i = minColumn; i < maxColumn + 1; ++i){
+            Collections.sort(map.get(i),(a,b)-> a[0]-b[0]);
+             List<Integer> temp = new ArrayList<>();
+            for(int[] val: map.get(i)){
+                temp.add(val[1]);
+            }
+            result.add(temp);
+        }
+        return result;
+    }
+    
+
+    
+    private void dfs(TreeNode root,int depth,int column,Map<Integer, List<int[]>> map ){
+        if(root == null)
+            return;
+        
+        if(!map.containsKey(column)){
+            map.put(column,new ArrayList<>());
+        }
+        
+        map.get(column).add(new int[]{depth,root.val});
+        minColumn = Math.min(minColumn, column);
+        maxColumn = Math.max(maxColumn, column);
+        dfs(root.left,depth+1,column-1,map);
+        dfs(root.right,depth+1,column+1,map);
     }
 }
