f9eed161eff43d504473dd50a1665353ad363a3a
Ramandeep-singh-bhatia <54952924+Ramandeep-singh-bhatia@users.noreply.github.com>
2025-05-03 15:10:53 -0700

    Time: 49 ms (44.64%) | Memory: 101.9 MB (73.63%) - LeetSync

--- 354-russian-doll-envelopes/russian-doll-envelopes.java
+++ 354-russian-doll-envelopes/russian-doll-envelopes.java
@@ -46,7 +46,23 @@ element 3 - 4 - It can attach behind - 0,1,0,2 and max is 3
 }*/
 
 /*
-    The above solution will be TLE because of large array length. We can try improve on the time complexity from n^2. In the above approach we maintain a array with the larget increasing subsequence length till that element. This apporach required us to loop through each element starting from 0 till that eleemnt to find the number of elements which are smaller than the current number and keep track of that in an array. In the end we can return the maximum value from that array. Another approach is to maintain the longest subsequence list If we find a number which is greater than the last number in the list, we can automatically add that number to the list. If not, we loop through the list and find the number which is larger than the current number and replace it with the number. Lest say we have first number as 6 and next we get 1, we know that we have a chance to get some numbers betweene 1 and 6 in the future, so it makes sense to maintain the list with the smaller number in the begning as we need to find the maximum length of incresing subsequence. This will still be O(n^2) but we can imporve on this if we do a binary search as the list we are maintaining will be sorted and we can easily do binary search to find the number that is just larger than the current number so that we cna replace it in the list with the current number.
+    The above solution will be TLE because of large array length. We can try improve on the time complexity from n^2. In the above approach we maintain a array with the larget increasing subsequence length till that element. This apporach required us to loop through each element starting from 0 till that eleemnt to find the number of elements which are smaller than the current number and keep track of that in an array. In the end we can return the maximum value from that array. Another approach is to maintain a list where l[i] represents the smallest possible value that can end an increasing subsequence of length i+1.
+    We are sorting the 2D array using width in increasing order. What happens when we get same width, If we try to sort the array for same width in increasing order and do LIS using height, we might end up getting unecessary elements that become eligible for LIS but it will give wrong result.
+    envelope - [[2,3], [5,4], [6,5], [6,7], [2,2]]
+    After sorting both increasing order - [[2,2], [2,3], [5,4], [6,5], [6,7]]
+    LIST using just height will be. 2,3,4,5,7 but this is wrong as 2,2 and 2,3 cannot be part of the LIS as width is same. Similarly 6,5 and 6,7 cannot be part of the list too.
+    One way to avoid this problem is to sort the 2d array with width in ascending and height in decending order
+    After sorting - [[2,3], [2,2], [5,4], [6,7], [6,5]]
+    When we get 2,3 it can be part of the LIS.and when we reach 2,2, we can skip it. This will form LIS using height - 3,4,7 and If you look at the dimentions, it should be able to fit the envelopes as the dimentions are as per the problem rule which is 2,3 - 5,4 - 6,7
+
+    But what if after sorting we have below scenario
+    After sorting - [[2,6], [2,1], [5,4], [6,7], [6,5]]
+    In this case the envelopes that we can fit will be 2,6 - 6,7 but technically if we use 2,1 we can fit in 5,4 and then 6,7 or 6,5 making LIS to 3.
+    This means if we have same width and we are sorting height in decending order, when we get a height smaller than the previous height, we can include that envelope so that it lets us fit more envelopes
+    
+    If we find a number which is greater than the last number in the list, we can automatically add that number to the list. If not, we loop through the list and find the number which is larger than the current number and replace it with the number. Why this will work is lets say we have the list [2,7,8] and we get a 5. Obviously 5 is smaller than 8 and will not contribute to the LIS. Next we get a 6 and it will still not contribute to LIS. But if we replaced 7 by 5, though it will not change the length of LIS but it give more options to fit in number sand eventually increasing the LIS length i.e. when we get 5 we canc ahneg the list to [2,5,8] and when we get 6, the LIST will be [2,5,6,8] This list will
+    
+     This will still be O(n^2) but we can imporve on this if we do a binary search as the list we are maintaining will be sorted and we can easily do binary search to find the number that is just larger than the current number so that we can replace it in the list with the current number.
 
     Time complexity - O(nlogn + n^2)
     Space complexity - O(n)
