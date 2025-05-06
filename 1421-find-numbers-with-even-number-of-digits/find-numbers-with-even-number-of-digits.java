5814d40f11c84eb6fb182775cfae59a1dcb5bab0
Ramandeep-singh-bhatia <54952924+Ramandeep-singh-bhatia@users.noreply.github.com>
2025-05-06 01:28:36 -0700

    Time: 1 ms (98.78%) | Memory: 43.1 MB (50.15%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
@@ -4,7 +4,7 @@ Let M be the average length of the number.
 Time - O(N*M) - N because of for loop and M because of while loop is isEven. After each iteration,we reduce the length by 1.
 */
 
-class Solution {
+/*class Solution {
 
     public int findNumbers(int[] nums) {
         // Counter to count the number of even digit integers
@@ -26,5 +26,24 @@ class Solution {
             num /= 10;
         }
         return count % 2 == 0;
+    }
+}*/
+
+/*
+ Since the constraint of number in nums array is such that, 1 <= nums[i] <= 10^5 
+ We can use this constraint to solve the problem
+*/
+
+class Solution {
+    public int findNumbers(int[] nums) {
+        // Counter to count the number of even digit integers
+        int result = 0;
+
+        for (int num : nums) {
+            if ((num >= 10 && num <= 99) || (num >= 1000 && num <= 9999) || num == 100000)
+                result++;
+        }
+
+        return result;
     }
 }
