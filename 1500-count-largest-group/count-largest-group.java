8999d5bef64a954ed5f03813cd7ebfa8a92b3528
Ramandeep-singh-bhatia <54952924+Ramandeep-singh-bhatia@users.noreply.github.com>
2025-05-06 11:03:04 -0700

    Time: 3 ms (98.14%) | Memory: 40.2 MB (95.25%) - LeetSync

--- 1500-count-largest-group/count-largest-group.java
+++ 1500-count-largest-group/count-largest-group.java
@@ -1,4 +1,9 @@
-class Solution {
+/*
+    Time - O(nlogn) base 10
+    Space - O(logn)
+*/
+
+/*class Solution {
 
     public int countLargestGroup(int n) {
         Map<Integer, Integer> map = new HashMap<Integer, Integer>();
@@ -20,5 +25,35 @@ class Solution {
             }
         }
         return count;
+    }
+}*/
+
+/*
+    If we look at the constriant 1 <= n <= 10^4 and we have to calculate the sum of the digits, the largest sum will come from 9999 which is 36. SO instead of using map we can use an array of size 37
+*/
+
+class Solution {
+    public int countLargestGroup(int n) {
+        if(n <= 9)
+            return n;
+        int max = 0;
+        int arr[] = new int[37];
+        for(int i=1; i<=n; i++) {
+            int sum = 0;
+            int j = i;
+            while (j != 0) {
+                sum += j % 10;
+                j /= 10;
+            }
+            arr[sum] = arr[sum] + 1;
+            max = Math.max(max,arr[sum]);
+        }
+        int count = 0;
+        for (int freq : arr) {
+            if (freq == max) 
+                count++;
+        }
+        return count;
+
     }
 }
