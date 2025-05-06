2db2ed14039b24f9495b23f93bf2d3404c254861
Ramandeep-singh-bhatia <54952924+Ramandeep-singh-bhatia@users.noreply.github.com>
2025-05-06 12:21:11 -0700

    Time: 0 ms (100.00%) | Memory: 41.9 MB (92.58%) - LeetSync

--- 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.java
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.java
@@ -13,7 +13,7 @@
  *     }
  * }
  */
-class Solution {
+/*class Solution {
     public boolean isCompleteTree(TreeNode root) {
         
         Queue<TreeNode> queue = new LinkedList<TreeNode>();
@@ -40,5 +40,29 @@ class Solution {
             prevNode = curNode;
         }
         return true;
+    }
+}*/
+
+/*
+    Count number of nodes in the tree. tree has a property where the left child is at 2*i position and right child is 2*i + 1 position for a parent at ith position.
+    We can keep track of the number of nodes and at any point the number of nodes are less than the index value for the node, we know there is a gap so we return false
+*/
+
+class Solution {
+    public int countNodes(TreeNode root){
+        if(root == null) 
+            return 0;
+        return 1 + countNodes(root.left) + countNodes(root.right);
+    }
+    public boolean dfs(TreeNode root,int total,int i){
+        if(root == null) 
+            return true;
+        if(i>total) 
+            return false;
+        return dfs(root.left,total,2*i) && dfs(root.right,total,2*i+1);
+    }
+    public boolean isCompleteTree(TreeNode root) {
+        int totalNodes = countNodes(root);
+        return dfs(root,totalNodes,1);
     }
 }
