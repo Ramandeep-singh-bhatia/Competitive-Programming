01caa1628e53bfc37fa0b9986796330ecdea1d21
Ramandeep-singh-bhatia <54952924+Ramandeep-singh-bhatia@users.noreply.github.com>
2025-05-06 01:04:15 -0700

    Time: 3 ms (97.89%) | Memory: 55.5 MB (43.91%) - LeetSync

--- 1165-meeting-scheduler/meeting-scheduler.java
+++ 1165-meeting-scheduler/meeting-scheduler.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public List<Integer> minAvailableDuration(int[][] slots1, int[][] slots2, int duration) {
         Arrays.sort(slots1, (a, b) -> a[0] - b[0]);
         Arrays.sort(slots2, (a, b) -> a[0] - b[0]);
@@ -17,6 +17,42 @@ class Solution {
                 pointer1++;
             } else {
                 pointer2++;
+            }
+        }
+        return new ArrayList<Integer>();
+    }
+}*/
+
+/*
+    Heap
+
+    Another approach of systematically selecting slots and comparing them is using a heap. We would initialize a heap timeslots and push all of the time slots into it.
+
+The key idea here is that we only need one heap. That is, we can put the time slots for both people into the same heap, and then if we find a common time slot, we know that the two-time slots couldn't possibly be for the same person.
+
+Time complexity: O((M+N)log(M+N)), when M is the length of slots1 and N is the length of slots2.
+
+There are two parts to be analyzed: 1) building up the heap; 2) the iteration when we keep popping elements from the heap. For the second part, popping one element takes O(log(M+N)), therefore, in the worst case, popping M+N elements takes O((M+N)log(M+N)).
+
+Space - O(M+N). This is because we store all M+N time slots in a heap.
+*/
+
+class Solution {
+    public List<Integer> minAvailableDuration(int[][] slots1, int[][] slots2, int duration) {
+        PriorityQueue<int[]> timeslots = new PriorityQueue<>((slot1, slot2) -> slot1[0] - slot2[0]);
+
+        for (int[] slot: slots1) {
+            if (slot[1] - slot[0] >= duration) timeslots.offer(slot);
+        }
+        for (int[] slot: slots2) {
+            if (slot[1] - slot[0] >= duration) timeslots.offer(slot);
+        }
+
+        while (timeslots.size() > 1) {
+            int[] slot1 = timeslots.poll();
+            int[] slot2 = timeslots.peek();
+            if (slot1[1] >= slot2[0] + duration) {
+                return new ArrayList<Integer>(Arrays.asList(slot2[0], slot2[0] + duration));
             }
         }
         return new ArrayList<Integer>();
