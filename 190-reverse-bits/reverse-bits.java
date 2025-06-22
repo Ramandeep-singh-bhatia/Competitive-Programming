85c5e2c7492f6f50fed75dcb0429aaf290ad5a17
Ramandeep-singh-bhatia <54952924+Ramandeep-singh-bhatia@users.noreply.github.com>
2025-06-21 19:27:57 -0700

    Time: 0 ms (100.00%) | Memory: 42 MB (45.32%) - LeetSync

--- 190-reverse-bits/reverse-bits.java
+++ 190-reverse-bits/reverse-bits.java
@@ -1,6 +1,20 @@
 public class Solution {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
+        int res = 0;
+        for(int i = 0; i < 32; i++){
+            res += n & 1;
+            n = n >>> 1;
+            if(i<31)
+                res = res << 1;
+        }
+        return res;
+    }
+}
+
+/*public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
         int result = 0; 
         int position = 31;
         while(n != 0) {
@@ -11,4 +25,4 @@ public class Solution {
 
         return result;
     }
-}
+}*/
