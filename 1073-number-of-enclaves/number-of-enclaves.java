3b7f67b255d7c74ced2af179290184ef10b39e06
Ramandeep-singh-bhatia <54952924+Ramandeep-singh-bhatia@users.noreply.github.com>
2025-07-02 13:19:35 -0700

    Time: 10 ms (31.24%) | Memory: 54.4 MB (76.31%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     int numBoundaryOnes = 0;
     int[][] directions = {{0,1}, {0,-1}, {1,0}, {-1,0}};
     public int numEnclaves(int[][] grid) {
@@ -39,5 +39,60 @@ class Solution {
                 dfs(grid, visited, ni, nj, m , n);
             }
         }
+    }
+}*/
+
+class Solution {
+    int[][] directions = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+    public int numEnclaves(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int numOfOnes = 0;
+        int numBoundaryOnes = 0;
+        
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 1){
+                    numOfOnes++;
+                }
+            }
+        }
+
+        boolean[][] visited = new boolean[m][n];
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 1 && (i == 0 || j == 0 || i == m-1 || j == n-1)){
+                    if(!visited[i][j]){
+                       numBoundaryOnes += bfs(grid, visited, i , j, m ,n);
+                    }
+                }
+            }
+        }
+
+        return numOfOnes - numBoundaryOnes;
+    }
+
+    private int bfs(int[][] grid, boolean[][] visited, int i , int j, int m, int n){
+        Queue<int[]> q = new LinkedList<>();
+        int count = 1;
+        q.add(new int[]{i,j});
+        visited[i][j] = true;
+        while(!q.isEmpty()){
+            int[] temp = q.poll();
+            
+            for(int[] dir: directions){
+                int ni = temp[0] + dir[0];
+                int nj = temp[1] + dir[1];
+
+                if(ni >= 0 && nj >= 0 && ni < m && nj < n && !visited[ni][nj] && grid[ni][nj] == 1){
+                    visited[ni][nj] = true;
+                    count++;
+                    q.add(new int[]{ni, nj});
+                }
+            }
+        }
+
+        return count;
     }
 }
