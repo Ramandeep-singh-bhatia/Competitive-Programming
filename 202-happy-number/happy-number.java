6b0ea648e2d1a472b71b3b6eb5148c92367646e5
Ramandeep-singh-bhatia <54952924+Ramandeep-singh-bhatia@users.noreply.github.com>
2025-07-13 17:23:14 -0700

    Time: 2 ms (18.89%) | Memory: 41.3 MB (9.26%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean isHappy(int n) {
+        
+        Set<Integer> s = new HashSet<>();
+        
+        while (!s.contains(n)){
+            s.add(n);   
+            n = square(n); 
+        }
+        
+        return n == 1;
+    }
+
+    private int square(int num) {
+        int ans = 0;
+        while(num > 0) {
+            int remainder = num % 10;
+            ans += remainder * remainder;
+            num /= 10;
+        }
+        return ans;
+    }
+}
+
+/*class Solution {
+    public boolean isHappy(int n) {
+        
+        int slow = n;
+        int fast = square(n);
+        while (slow != fast) {
+            slow = square(slow);
+            fast = square(square(fast));
+        } 
+
+        return slow == 1;
+    }
+
+    private int square(int num) {
+        int ans = 0;
+        while(num > 0) {
+            int remainder = num % 10;
+            ans += remainder * remainder;
+            num /= 10;
+        }
+        return ans;
+    }
+}*/
