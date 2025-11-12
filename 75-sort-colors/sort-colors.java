eb65f79011bf234489b0c6bb1952d037b73003d6
Ramandeep-singh-bhatia <54952924+Ramandeep-singh-bhatia@users.noreply.github.com>
2025-11-11 20:44:53 -0800

    Time: 0 ms (100.00%) | Memory: 43.2 MB (13.19%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public void sortColors(int[] nums) {
         int n = nums.length;
         int[] count = new int[3];
@@ -13,5 +13,33 @@ class Solution {
                 count[j]--;
             }
         }
+    }
+}*/
+
+/*
+    Above code is O(2n) time complexity. We can implement dutch flag algorithm for O(n) time complexity and O(1) space complexity
+*/
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int low = 0, mid = 0, high = nums.length - 1;
+        while (mid <= high){
+            if(nums[mid] == 0){
+                swap(nums, mid, low);
+                mid++;
+                low++;
+            } else if(nums[mid] == 1){
+                mid++;
+            } else {
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+
+    private void swap (int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
 }
