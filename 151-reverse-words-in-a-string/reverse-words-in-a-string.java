bbe1f1580424a5ee43f8d812c865ce63ed424b21
Ramandeep Singh <54952924+Ramandeep-singh-bhatia@users.noreply.github.com>
2025-12-04 23:25:35 -0800

    Time: 8 ms (37.99%) | Memory: 44.7 MB (27.62%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -0,0 +1,50 @@
+/*
+The input string s is trimmed to remove any leading or trailing spaces using the trim() method.
+The trimmed string is then split into an array of words using the split("\\s+") method. The regular expression "\s+" matches one or more whitespace characters, effectively separating the words.
+*/
+
+/*class Solution {
+    public String reverseWords(String s) {
+        String[] arr = s.trim().split("\\s+");
+        String result = "";
+
+        for(int i = arr.length - 1; i >= 0; i--){
+            result += arr[i] + " ";
+        }
+
+        return result.trim();
+    }
+}*/
+
+class Solution {
+    public String reverseWords(String s) {
+        int l = 0;
+        int r = s.length() - 1;
+        while(l <= r && s.charAt(l) == ' '){
+                ++l;
+        }
+
+        while(l <= r && s.charAt(r) == ' '){
+                --r;
+        }
+
+        StringBuilder word = new StringBuilder();
+        Deque<String> result = new ArrayDeque<>();
+
+        while(l <= r){
+            char c = s.charAt(l);
+            if(c != ' '){
+                word.append(c);
+            } else if((word.length() != 0) && (c == ' ')){
+                result.offerFirst(word.toString());
+                word = new StringBuilder();
+            }
+            ++l;
+        }
+
+        result.offerFirst(word.toString());
+
+        return String.join(" ", result);
+
+    }
+}
