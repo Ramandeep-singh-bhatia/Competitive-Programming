6e2f5e0f31e66578abc5f62c968379a2acc55071
Ramandeep Singh <54952924+Ramandeep-singh-bhatia@users.noreply.github.com>
2026-01-29 16:05:11 -0800

    Time: 1 ms (89.22%) | Memory: 44.5 MB (52.94%) - LeetSync

--- 360-sort-transformed-array/sort-transformed-array.java
+++ 360-sort-transformed-array/sort-transformed-array.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
         int n = nums.length;
         int[] result = new int[n];
@@ -11,4 +11,54 @@ class Solution {
 
         return result;
     }
+}*/
+
+class Solution {
+    public int[] sortTransformedArray(int[] nums, int a, int b, int c) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int index = 0;
+        int left = 0;
+        int right = n - 1;
+
+        if( a >= 0) {
+            index = n - 1;
+            while(left <= right) {
+                int leftval = helper(nums[left], a, b, c);
+                int rightval = helper(nums[right], a, b, c);
+
+                if(leftval >= rightval){
+                    result[index] = leftval;
+                    left++;
+                } else {
+                    result[index] = rightval;
+                    right--;
+                }
+
+                index--;
+            }
+        } else {
+            while(left <= right){
+                int leftval = helper(nums[left], a, b, c);
+                int rightval = helper(nums[right], a, b, c);
+
+                if(leftval <= rightval){
+                    result[index] = leftval;
+                    left++;
+                } else {
+                    result[index] = rightval;
+                    right--;
+                }
+
+                index++;
+            }
+        }
+        return result;
+    }
+
+    private int helper(int x, int a, int b, int c){
+        return a * x * x + b * x + c;
+    }
 }
+
