8ac3eb3144e4eed5f60c32ae722b39ead8b8b796
Ramandeep Singh <54952924+Ramandeep-singh-bhatia@users.noreply.github.com>
2026-02-02 00:26:29 -0800

    Time: 1 ms (87.68%) | Memory: 47.1 MB (30.06%) - LeetSync

--- 272-closest-binary-search-tree-value-ii/closest-binary-search-tree-value-ii.java
+++ 272-closest-binary-search-tree-value-ii/closest-binary-search-tree-value-ii.java
@@ -13,7 +13,7 @@
  *     }
  * }
  */
-class Solution {
+/*class Solution {
     public List<Integer> closestKValues(TreeNode root, double target, int k) {
         List<Integer> result = new ArrayList<>();
         List<Integer> distance = new ArrayList<>();
@@ -35,5 +35,67 @@ class Solution {
         inorder(node.left, distance);
         distance.add(node.val);
         inorder(node.right, distance);
+    }
+}*/
+
+
+class Solution {
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        List<Integer> result = new ArrayList<>();
+        List<Integer> sortedValues = new ArrayList<>();
+        inorder(root, sortedValues);
+
+        int index = binarySearch(sortedValues, target);
+
+        int left = index - 1;
+        int right = index;
+
+        while(result.size() < k){
+            if(left < 0){
+                result.add(sortedValues.get(right));
+                right++;
+            } else if(right >= sortedValues.size()) {
+                result.add(sortedValues.get(left));
+                left--;
+            } else {
+                double leftDist = Math.abs(sortedValues.get(left) - target);
+                double rightDist = Math.abs(sortedValues.get(right) - target);
+
+                if(leftDist <= rightDist){
+                    result.add(sortedValues.get(left));
+                    left--;
+                } else {
+                    result.add(sortedValues.get(right));
+                    right++;
+                }
+            }
+
+        }
+
+        return result;
+    }
+
+    private int binarySearch(List<Integer> l, double target){
+        int left = 0;
+        int right = l.size();
+
+        while(left < right) {
+            int mid = left + (right - left) / 2;
+            if(l.get(mid) < target){
+                left = mid + 1;
+            } else
+                right = mid;
+        }
+
+        return left;
+    }
+
+    private void inorder(TreeNode node, List<Integer> sortedValues){
+        if(node == null)
+            return;
+
+        inorder(node.left, sortedValues);
+        sortedValues.add(node.val);
+        inorder(node.right, sortedValues);
     }
 }
